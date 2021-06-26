029b994c46231e431dff0accce8caa997f0b7c40
luis-luciqm <redbullfc18@gmail.com>
2021-06-26 15:53:52 -0300

    Analisys in folders and files

--- src/main/java/com/mycompany/monitoring/ObervaPasta.java
+++ src/main/java/com/mycompany/monitoring/ObervaPasta.java
@@ -1,35 +1,65 @@
 package com.mycompany.monitoring;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
-import static java.lang.Thread.sleep;
+//import static java.lang.Thread.sleep;
 
 public class ObervaPasta extends Thread{
     public static void getFileNameFolder(){
+        
         while(true){
             String arquivos = "";
             int cont = 0;
 
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("/home/luis/NetBeansProjects/Monitoring/Pasta1"))) {
                 for (Path file: stream) {
-                    arquivos += (file.getFileName());
-                    cont++;
-                }
+                    arquivos += (file.getFileName() + ";");
+                    
+//                    System.out.println(file);
+                    
+//                    file.getFileName() exibe apenas o arquivo
+//                    file exibe o caminho + o arquivo.txt
+                    
+                    /*
+                    
+                    cada iteração, file é APENAS UM ARQUIVO
+                    logica: pegar cada arquivo de uma vez só
+                    esperar x segundos para administrar esse arquivo, uns 10 segundos
+                    
+                    pegando o arquivo, devemos move-lo para a pasta 2
+                    
+                    fazer a leitura e operação matematica
+                    
+                    */
+                    
+//                    System.out.println(arquivos);
 
+                    try{
+                        Thread.sleep(5000);
+                    }catch(InterruptedException excep){
+                        System.out.println(excep.getMessage());
+                    }
+                    
+                }
             } catch (IOException | DirectoryIteratorException ex) {
                 System.err.println(ex);
             }
 
             if(arquivos.equals("")){
                 System.out.println("Nada");
-            }else{
-                System.out.println(arquivos); // pq esse omh  n enviou nenhum arquivo, pere ai, pronto testar agr
+                
+            }else{ // retirar o else após mover o arquivo com a função, n tem sentido deixar
+                System.out.println(arquivos);
                 moveArquivo("/home/luis/NetBeansProjects/Monitoring/Pasta1/" + arquivos); //pasta2 ou 1 ?f
             }
+            
             /* perai, antes bora ver se ele move, como escolho onde ele vai mover?
             *
             *
@@ -51,7 +81,26 @@ public class ObervaPasta extends Thread{
             }
         }
     }
-                                                //
+
+
+    public static void filaDeArquivos(Path file){ // caso sera inserido mais de dois arquivos
+        
+    }
+    
+//    inicio função escrever1()
+    public static void escreve(){
+        FileWriter fw = null;
+        try{
+            fw = new FileWriter("ARQUIVO_AQUI.txt");
+            fw.write("kss"); // aqui será escrito o arquivo que foi lido anteriormente
+            fw.close();
+            System.out.println("Salvo com sucesso");
+        }catch(IOException exe){
+            System.out.println("Erro ao escrever no arquivo: " + exe.getMessage());
+        }
+    }
+
+
     public static void moveArquivo(String nome){ //
         // Arquivo a ser movido, tem que ser static msm
         File arquivo = new File(nome);
